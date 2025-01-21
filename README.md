e659fe992089c1dcf09ab96930b13a49991dc12c
Matronator <5470780+matronator@users.noreply.github.com>
2025-01-21 05:19:18 +0100

    update to bun

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ Showcase a different repository every week.
 <h2 align="center">
   Repo of the week:
 </h2>
-<h3 align="center"><a href="https://github.com/matronator/pwa-template">pwa-template</a></h3>
+<h3 align="center"><a href="https://github.com/matronator/MTRGen">MTRGen</a></h3>
 <p align="center">
-  <a href="https://github.com/matronator/pwa-template">
-    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=matronator&repo=pwa-template&title_color=bf1f1f&icon_color=ffbf00&text_color=ffffff&bg_color=100,000000,360428,730517" alt="matronator/pwa-template">
+  <a href="https://github.com/matronator/MTRGen">
+    <img align="center" src="https://github-readme-stats.vercel.app/api/pin/?username=matronator&repo=MTRGen&title_color=bf1f1f&icon_color=ffbf00&text_color=ffffff&bg_color=100,000000,360428,730517" alt="matronator/MTRGen">
   </a>
 </p>
-<p align="center">Basic PWA setup for my future projects</p>
+<p align="center">File generator engine that can generate PHP files from JSON/YAML/NEON templates.</p>
 
 <!-- end repo of the week -->
--- main.ts
+++ main.ts
@@ -1,17 +1,26 @@
-import fs from 'fs/promises';
-import fetch from 'node-fetch';
+type RepositoryDataType = {
+  id: number;
+  name: string;
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  fork: boolean;
+};
 
 const API_URL = "https://api.github.com";
 
 try {
     const username = process.env.GH_USER ?? "matronator";
-    const url = `${API_URL}/users/${username}/repos?per_page=20&sort=stars`;
+    const url = `${API_URL}/users/${username}/repos?per_page=200&sort=updated`;
     const response = await fetch(url);
-    const repos = await response.json();
+    let repos: RepositoryDataType[] = await response.json();
+    repos = repos.filter(repo => !repo.fork);
+    repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    repos = repos.slice(0, 30);
     const repoOfTheWeek = repos[Math.floor(Math.random() * (repos.length - 1))];
 
     let updates = `<!-- start repo of the week -->\n\n`;
-    const { name, full_name, description } = repoOfTheWeek;
+    const { name, full_name, description, stargazers_count } = repoOfTheWeek;
     const template = `<h2 align="center">
   Repo of the week:
 </h2>
@@ -27,11 +36,12 @@ try {
     updates = updates.concat('<!-- end repo of the week -->');
 
     // Rewrite README with new post content
-    const currentText = await fs.readFile('README.md', 'utf8');
+    const currentTextFile = Bun.file('README.md');
+    const currentText = await currentTextFile.text();
     const repoSection = /<!-- start repo of the week -->[\s\S]*<!-- end repo of the week -->/g;
     const newText = currentText.replace(repoSection, updates);
 
-    await fs.writeFile('README.md', newText);
+    await Bun.write('README.md', newText);
 } catch (error) {
   console.error('there was an error:', error.message);
 }
